d0b5de20211ea5de0363ee4477e06696f329c538
Jon Ham <23408816+jondwoo@users.noreply.github.com>
2023-03-08 02:08:45 -0800

    Update keymap.c

--- tkc/osav2/keymaps/jondwoo/keymap.c
+++ tkc/osav2/keymaps/jondwoo/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ┌───┐ ┌───┬───┬───┬───┬───┬───┬───┐         ┌───┬───┬───┬───┬───┬───┬───┬───┐
  * │ESC│ │~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│         │& 7│* 8│( 9│) 0│_ -│+ =│BS │BS │
  * ├───┤ ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┘       ┌─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤
- * │   │ │Tab  │" '│ , │ . │ P │ Y │         │ F │ G │ C │ R │ L │{ [│} ]│ | \ │
+ * │   │ │Tab  │" '│ , │ . │ P │ Y │         │ F │ G │ C │ R │ L │/ ?│} ]│ | \ │
  * ├───┤ ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┐        └┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
  * │Bse│ │Caps  │ A │ O │ E │ U │ I │         │ D │ H │ T │ N │ S │ - │   Enter│
  * └───┘ ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┐     ┌─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤
@@ -60,8 +60,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_WORK] = LAYOUT_all(
     KC_ESC,    KC_GRV,  KC_1,         KC_2,         KC_3,                KC_4,         KC_5,       KC_6,    KC_7,         KC_8,         KC_9,         KC_0,         KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,
-    _______,   KC_TAB,  KC_QUOT,      KC_COMM,      KC_DOT,              KC_P,         KC_Y,       KC_F,    KC_G,         KC_C,         KC_R,         KC_L,         KC_LBRC, KC_RBRC, KC_BSLS,
-    TG(_WORK), KC_CAPS, LGUI_T(KC_A), LALT_T(KC_O), LCTL_T(KC_E),        LSFT_T(KC_U), KC_I,       KC_D,    RSFT_T(KC_H), RCMD_T(KC_T), LOPT_T(KC_N), RCTL_T(KC_S), KC_MINS, KC_ENT,
+    _______,   KC_TAB,  KC_QUOT,      KC_COMM,      KC_DOT,              KC_P,         KC_Y,       KC_F,    KC_G,         KC_C,         KC_R,         KC_L,         KC_SLSH, KC_RBRC, KC_BSLS,
+    TG(_WORK), KC_CAPS, LCTL_T(KC_A), LOPT_T(KC_O), LCMD_T(KC_E),        LSFT_T(KC_U), KC_I,       KC_D,    RSFT_T(KC_H), RCMD_T(KC_T), LOPT_T(KC_N), RCTL_T(KC_S), KC_MINS, KC_ENT,
                KC_LSFT, KC_SCLN,      KC_Q,         KC_J,                KC_K,         KC_X,       KC_X,    KC_B,         KC_M,         KC_W,         KC_V,         KC_Z,    KC_RSFT, MO(_LOWER),
                KC_LCMD,               KC_LOPT,      LT(_LOWER, KC_SPC),  MO(_RAISE),               KC_SPC,  KC_RCMD,                                                         _______
 ),
